5588206d6060546c22b1e3174430d55e92863d33
Sebastiano Verdolini <sebastiano.verdolini@gmail.com>
2019-04-16 11:11:23 +0200

    Implement Research CreateView

--- hansberger/research/views.py
+++ hansberger/research/views.py
@@ -2,3 +2,8 @@ from django.views.generic import (
     CreateView,
 )
 from .models import Research
+
+
+class ResearchCreateView(CreateView):
+    model = Research
+    fields = ['name', 'description']
