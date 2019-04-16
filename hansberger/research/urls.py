4c84f99f1a2f591ed251d9e92803ef1b2cb6f9a8
Sebastiano Verdolini <sebastiano.verdolini@gmail.com>
2019-04-16 12:44:38 +0200

    Add detail and delete urls

--- hansberger/research/urls.py
+++ hansberger/research/urls.py
@@ -11,4 +11,6 @@ app_name = "research"
 urlpatterns = [
     path("", view=ResearchListView.as_view(), name="research-list"),
     path("new/", view=ResearchCreateView.as_view(), name="research-create"),
+    path("<slug:research_slug>", view=ResearchDetailView.as_view(), name="research-detail"),
+    path("<slug:research_slug>/delete", view=ResearchDeleteView.as_view(), name="research-delete"),
 ]
