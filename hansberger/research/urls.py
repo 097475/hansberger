527de0c11225384315fc02a69ffbc0b53965d1c0
Sebastiano Verdolini <sebastiano.verdolini@gmail.com>
2019-04-16 15:46:04 +0200

    Add Dataset views urls

--- hansberger/research/urls.py
+++ hansberger/research/urls.py
@@ -4,6 +4,8 @@ from .views import (
     ResearchDeleteView,
     ResearchDetailView,
     ResearchListView,
+    DatasetDetailView,
+    DatasetListView,
 )
 
 
@@ -11,6 +13,8 @@ app_name = "research"
 urlpatterns = [
     path("", view=ResearchListView.as_view(), name="research-list"),
     path("new/", view=ResearchCreateView.as_view(), name="research-create"),
-    path("<slug:research_slug>", view=ResearchDetailView.as_view(), name="research-detail"),
-    path("<slug:research_slug>/delete", view=ResearchDeleteView.as_view(), name="research-delete"),
+    path("<slug:research_slug>/", view=ResearchDetailView.as_view(), name="research-detail"),
+    path("<slug:research_slug>/delete/", view=ResearchDeleteView.as_view(), name="research-delete"),
+    path("<slug:research_slug>/datasets/", view=DatasetListView.as_view(), name="dataset-list"),
+    path("<slug:research_slug>/datasets/<slug:dataset_slug>/", view=DatasetDetailView.as_view(), name="dataset-detail"),
 ]
