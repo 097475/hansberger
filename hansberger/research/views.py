bc1f21d9c36cc8a5c65bc597992be36fe80abb69
Sebastiano Verdolini <sebastiano.verdolini@gmail.com>
2019-04-16 11:13:17 +0200

    Implement Research DeleteView

--- hansberger/research/views.py
+++ hansberger/research/views.py
@@ -1,9 +1,19 @@
 from django.views.generic import (
     CreateView,
+    DeleteView,
 )
+from django.urls import reverse_lazy
 from .models import Research
 
 
 class ResearchCreateView(CreateView):
     model = Research
     fields = ['name', 'description']
+
+
+class ResearchDeleteView(DeleteView):
+    model = Research
+    context_object_name = 'research'
+    slug_field = 'slug'
+    slug_url_kwarg = 'research_slug'
+    success_url = reverse_lazy('research: research-list')
