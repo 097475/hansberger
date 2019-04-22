547cc0889e291987c5d9fc801c8354047addcbbc
Sebastiano Verdolini <sebastiano.verdolini@gmail.com>
2019-04-22 16:46:56 +0200

    Adapt imports to new name

--- hansberger/research/models/__init__.py
+++ hansberger/research/models/__init__.py
@@ -1,3 +1,3 @@
 from .research import Research # noqa
 from .dataset import Dataset # noqa
-from .source import EdfSource, TextSource # noqa
+from .data_source import EdfDataSource, TextDataSource # noqa
