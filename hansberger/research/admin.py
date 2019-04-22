53e03b01bf7dc6babc6c32ec5623dc8dc47da55c
Sebastiano Verdolini <sebastiano.verdolini@gmail.com>
2019-04-22 16:33:08 +0200

    Register models

--- hansberger/research/admin.py
+++ hansberger/research/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Research, EdfSource, TextSource, Dataset
 
-# Register your models here.
+admin.site.register(Research)
+admin.site.register(EdfSource)
+admin.site.register(TextSource)
+admin.site.register(Dataset)
