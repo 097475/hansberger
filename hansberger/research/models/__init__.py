2dbc392146ae8dc6739cc6ea096a7577e1906bef
Sebastiano Verdolini <sebastiano.verdolini@gmail.com>
2019-04-24 15:15:35 +0200

    Adjust imports

--- hansberger/research/models/__init__.py
+++ hansberger/research/models/__init__.py
@@ -1,2 +1,4 @@
 from .research import Research # noqa
-from .dataset import Dataset, EDFDataset, TextDataset # noqa
+from .dataset import Dataset
+from .edf_dataset import EDFDataset
+from .text_dataset import TextDataset
