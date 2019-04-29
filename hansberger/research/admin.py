e3f96333455c0a740ac79b8bfec6731e08a71e4b
Sebastiano Verdolini <sebastiano.verdolini@gmail.com>
2019-04-29 18:54:30 +0200

    Register model in admin

--- hansberger/research/admin.py
+++ hansberger/research/admin.py
@@ -2,7 +2,17 @@ from django.contrib import admin
 from .models import (
     Research,
     Dataset,
+    EDFDataset,
+    TextDataset,
+    FiltrationAnalysis,
+    VietorisFiltrationAnalysis,
+    CliqueWeightedRankFiltrationAnalysis,
 )
 
 admin.site.register(Research)
 admin.site.register(Dataset)
+admin.site.register(EDFDataset)
+admin.site.register(TextDataset)
+admin.site.register(FiltrationAnalysis)
+admin.site.register(VietorisFiltrationAnalysis)
+admin.site.register(CliqueWeightedRankFiltrationAnalysis)
