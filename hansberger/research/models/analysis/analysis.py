be9c2d3526b4a3f579265870dea65c1018d64cce
Sebastiano Verdolini <sebastiano.verdolini@gmail.com>
2019-04-29 19:18:22 +0200

    Refactor naming, formatting and insert dataset relation

--- hansberger/research/models/analysis/analysis.py
+++ hansberger/research/models/analysis/analysis.py
@@ -1,8 +1,9 @@
+from math import inf
 from django.contrib.postgres.fields import JSONField
 from django.db import models
 from django.utils.text import slugify
-import math
-from .research import Research
+from ..research import Research
+from ..dataset.dataset import Dataset
 
 
 class Analysis(models.Model):
@@ -13,6 +14,8 @@ class Analysis(models.Model):
     research = models.ForeignKey(
         Research,
         on_delete=models.CASCADE,
+        related_name='analysis_set',
+        related_query_name='analysis',
     )
 
     class Meta:
@@ -29,8 +32,8 @@ class Analysis(models.Model):
 
 
 class FiltrationAnalysis(Analysis):
-    VIETORIS_RIPS_FILTRATION = 'VietorisRipsFiltration'
-    CLIQUE_WEIGHTED_RANK_FILTRATION = 'CliqueWeightedRankFiltration'
+    VIETORIS_RIPS_FILTRATION = 'VRF'
+    CLIQUE_WEIGHTED_RANK_FILTRATION = 'CWRF'
 
     FILTRATION_TYPE_CHOICES = (
         (VIETORIS_RIPS_FILTRATION, 'Vietoris Rips Filtration'),
@@ -38,13 +41,35 @@ class FiltrationAnalysis(Analysis):
     )
 
     METRIC_CHOICES = (
-        ('braycurtis', 'Braycurtis'), ('canberra', 'Canberra'), ('chebyshev', 'Chebyshev'), ('cityblock', 'City block'),
-        ('correlation', 'Correlation'), ('cosine', 'Cosine'), ('dice', 'Dice'), ('euclidean', 'Euclidean'),
-        ('hamming', 'Hamming'), ('jaccard', 'Jaccard'), ('jensenshannon', 'Jensen Shannon'), ('kulsinski', 'Kulsinski'),
-        ('mahalanobis', 'Mahalonobis'), ('matching', 'Matching'), ('minkowski', 'Minkowski'),
-        ('rogerstanimoto', 'Rogers-Tanimoto'), ('russellrao', 'Russel Rao'), ('seuclidean', 'Seuclidean'),
-        ('sokalmichener', 'Sojal-Michener'), ('sokalsneath', 'Sokal-Sneath'), ('sqeuclidean', 'Sqeuclidean'),
-        ('yule', 'Yule')
+        ('braycurtis', 'Braycurtis'),
+        ('canberra', 'Canberra'),
+        ('chebyshev', 'Chebyshev'),
+        ('cityblock', 'City block'),
+        ('correlation', 'Correlation'),
+        ('cosine', 'Cosine'),
+        ('dice', 'Dice'),
+        ('euclidean', 'Euclidean'),
+        ('hamming', 'Hamming'),
+        ('jaccard', 'Jaccard'),
+        ('jensenshannon', 'Jensen Shannon'),
+        ('kulsinski', 'Kulsinski'),
+        ('mahalanobis', 'Mahalonobis'),
+        ('matching', 'Matching'),
+        ('minkowski', 'Minkowski'),
+        ('rogerstanimoto', 'Rogers-Tanimoto'),
+        ('russellrao', 'Russel Rao'),
+        ('seuclidean', 'Seuclidean'),
+        ('sokalmichener', 'Sojal-Michener'),
+        ('sokalsneath', 'Sokal-Sneath'),
+        ('sqeuclidean', 'Sqeuclidean'),
+        ('yule', 'Yule'),
+    )
+
+    source_dataset = models.ForeignKey(
+        Dataset,
+        on_delete=models.PROTECT,
+        related_name='filtration_analysis_set',
+        related_query_name='filtration_analysis'
     )
 
     filtration_type = models.CharField(
@@ -56,10 +81,10 @@ class FiltrationAnalysis(Analysis):
         choices=METRIC_CHOICES
     )
     max_homology_dimension = models.IntegerField(default=1)
-    max_distances_considered = models.FloatField(default=math.inf)
+    max_distances_considered = models.FloatField(default=inf)
     coeff = models.IntegerField(default=2)
     do_cocycles = models.BooleanField(default=False)
     n_perm = models.IntegerField(default=None, null=True)
 
-    analysis_result_matrix = JSONField(blank=True, null=True)
-    plot = models.ImageField(blank=True, null=True)
+    result_matrix = JSONField(blank=True, null=True)
+    result_plot = models.ImageField(blank=True, null=True)
