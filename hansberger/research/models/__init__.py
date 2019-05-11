31710459f453de00d4217d9f52f5f98ad2f91a7e
Sebastiano Verdolini <sebastiano.verdolini@gmail.com>
2019-05-11 10:30:04 +0200

    Update __init__

--- hansberger/research/models/__init__.py
+++ hansberger/research/models/__init__.py
@@ -1,6 +1,4 @@
 from .research import * # noqa
-from .dataset.dataset import * # noqa
-from .dataset.edf_dataset import * # noqa
-from .dataset.text_dataset import * # noqa
-from .analysis.analysis import * # noqa
-from .analysis.window import * # noqa
+from .dataset import * # noqa
+from .analysis import * # noqa
+from .window import * # noqa
