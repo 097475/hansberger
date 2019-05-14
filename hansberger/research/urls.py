8dd6097f6099f30f91362f3804185980fda6ffc9
Sebastiano Verdolini <sebastiano.verdolini@gmail.com>
2019-05-14 18:20:52 +0200

    Import views and register urls

--- hansberger/research/urls.py
+++ hansberger/research/urls.py
@@ -1,2 +1,16 @@
+from django.urls import path
+from .views import (
+    ResearchCreateView,
+    ResearchDeleteView,
+    ResearchDetailView,
+    ResearchListView,
+)
+
+
 app_name = 'research'
-urlpatterns = []
+urlpatterns = [
+    path("", view=ResearchListView.as_view(), name="research-list"),
+    path("new/", view=ResearchCreateView.as_view(), name="research-create"),
+    path("<slug:research_slug>/", view=ResearchDetailView.as_view(), name="research-detail"),
+    path("<slug:research_slug>/delete/", view=ResearchDeleteView.as_view(), name="research-delete"),
+]
