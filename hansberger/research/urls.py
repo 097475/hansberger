811140c1c9b77cfef462981d8e8460045a955554
097475 <matteo.belenchia@studenti.unicam.it>
2019-05-27 19:05:17 +0200

    added links; refactored urls

--- hansberger/research/urls.py
+++ hansberger/research/urls.py
@@ -34,8 +34,8 @@ urlpatterns = [
     path("<slug:research_slug>/datasets/<slug:dataset_slug>/process/", view=DatasetProcessRedirectView.as_view(), name="dataset-process-redirect"), # noqa
     path("<slug:research_slug>/datasets/<slug:dataset_slug>/process-text/", view=TextDatasetProcessFormView.as_view(), name="dataset-process-text"), # noqa
     path("<slug:research_slug>/datasets/<slug:dataset_slug>/delete/", view=DatasetDeleteView.as_view(), name="dataset-delete"), # noqa
-    path("<slug:research_slug>/filtrationanalysis/add/", view=FiltrationAnalysisCreateView.as_view(), name="filtrationanalysis-create"), # noqa
-    path("<slug:research_slug>/mapperanalysis/add/", view=MapperAnalysisCreateView.as_view(), name="mapperanalysis-create"), # noqa
+    path("<slug:research_slug>/analysis/filtrationanalysis/add/", view=FiltrationAnalysisCreateView.as_view(), name="filtrationanalysis-create"), # noqa
+    path("<slug:research_slug>/analysis/mapperanalysis/add/", view=MapperAnalysisCreateView.as_view(), name="mapperanalysis-create"), # noqa
     path("<slug:research_slug>/analysis/", view=AnalysisListView.as_view(), name="analysis-list"),
     path("<slug:research_slug>/analysis/<slug:analysis_slug>/", view=AnalysisDetailView.as_view(), name="analysis-detail"), # noqa 
     path("<slug:research_slug>/analysis/<slug:analysis_slug>/windows/", view=WindowListView.as_view(), name="window-list"), # noqa
