ea5fd0edf6453e2705be029e561b3946667922db
Sebastiano Verdolini <sebastiano.verdolini@gmail.com>
2019-05-28 19:43:30 +0200

    Remove list view url

--- hansberger/datasets/urls.py
+++ hansberger/datasets/urls.py
@@ -6,7 +6,6 @@ from .views import (
 
 app_name = 'datasets'
 urlpatterns = [
-    path('', view=DatasetListView.as_view(), name='dataset-list'),
     path('text/create/', view=TextDatasetCreateView.as_view(), name='text-dataset-create'),
     path('text/<slug:dataset_slug>/', view=TextDatasetDetailView.as_view(), name='text-dataset-detail'),
 ]
