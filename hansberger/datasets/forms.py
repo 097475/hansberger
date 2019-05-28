c758d0292af0f50adae17612ce88ea885dd1f9e7
Sebastiano Verdolini <sebastiano.verdolini@gmail.com>
2019-05-28 19:42:29 +0200

    Implement forms of TextDataset

---
+++ hansberger/datasets/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from .models import TextDataset
+
+
+class DatasetCreationMixin:
+    def __init__(self, *args, **kwargs):
+        research = kwargs.pop('research')
+        super().__init__(*args, **kwargs)
+        self.fields['research'].initial = research
+
+    class Meta:
+        widgets = {'research': forms.HiddenInput}
+
+
+class TextDatasetCreationForm(DatasetCreationMixin, forms.ModelForm):
+    class Meta(DatasetCreationMixin.Meta):
+        model = TextDataset
+        fields = [
+            'name',
+            'description',
+            'source',
+            'research',
+            'values_separator_character',
+            'identity_column_index',
+            'header_row_index',
+        ]
