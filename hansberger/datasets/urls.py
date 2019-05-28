6dbf9c86c46768d6082a33ca9b4614536a97e5db
Sebastiano Verdolini <sebastiano.verdolini@gmail.com>
2019-05-28 22:43:36 +0200

    Register list and redirect urls

--- hansberger/datasets/urls.py
+++ hansberger/datasets/urls.py
@@ -2,10 +2,14 @@ from django.urls import path
 from .views import (
     TextDatasetCreateView,
     TextDatasetDetailView,
+    DatasetListView,
+    DatasetRedirectView,
 )
 
 app_name = 'datasets'
 urlpatterns = [
+    path('', view=DatasetListView.as_view(), name='dataset-list'),
+    path('<slug:dataset_slug>/', view=DatasetRedirectView.as_view(), name='dataset-redirect'),
     path('text/create/', view=TextDatasetCreateView.as_view(), name='text-dataset-create'),
     path('text/<slug:dataset_slug>/', view=TextDatasetDetailView.as_view(), name='text-dataset-detail'),
 ]
