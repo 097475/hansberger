4b58fb0a525a5af97712af30ab820143d6d668fc
Sebastiano Verdolini <sebastiano.verdolini@gmail.com>
2019-05-28 16:42:41 +0200

    Change new with create in create research url

--- hansberger/research/urls.py
+++ hansberger/research/urls.py
@@ -9,7 +9,7 @@ from .views import (
 app_name = 'research'
 urlpatterns = [
     path("", view=ResearchListView.as_view(), name="research-list"),
-    path("new/", view=ResearchCreateView.as_view(), name="research-create"),
+    path("create/", view=ResearchCreateView.as_view(), name="research-create"),
     path("<slug:research_slug>/", view=ResearchDetailView.as_view(), name="research-detail"),
     path("<slug:research_slug>/delete/", view=ResearchDeleteView.as_view(), name="research-delete"),
 ]
