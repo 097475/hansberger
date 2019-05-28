a0903ce8787451f718cee74c35f96397b30d6791
Sebastiano Verdolini <sebastiano.verdolini@gmail.com>
2019-05-28 16:26:04 +0200

    Implement views of Research

--- hansberger/research/views.py
+++ hansberger/research/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    DetailView,
+    ListView,
+)
+from django.urls import reverse_lazy
+from .models import Research
 
-# Create your views here.
+
+class ResearchCreateView(CreateView):
+    model = Research
+    fields = ['name', 'description']
+
+
+class ResearchDeleteView(DeleteView):
+    model = Research
+    context_object_name = 'research'
+    slug_field = 'slug'
+    slug_url_kwarg = 'research_slug'
+    success_url = reverse_lazy('research:research-list')
+
+
+class ResearchDetailView(DetailView):
+    model = Research
+    context_object_name = 'research'
+    slug_field = 'slug'
+    slug_url_kwarg = 'research_slug'
+
+
+class ResearchListView(ListView):
+    model = Research
+    context_object_name = 'research_list'
+    queryset = Research.objects.all().values('name', 'creation_date', 'slug')
+    template_name = "research/research_list.html"
