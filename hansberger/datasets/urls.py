ec2637a56b3580eda51503bac97b1ff44d470861
Sebastiano Verdolini <sebastiano.verdolini@gmail.com>
2019-05-28 19:42:11 +0200

    Create dataset urls

---
+++ hansberger/datasets/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import (
+    TextDatasetCreateView,
+    TextDatasetDetailView,
+    DatasetListView,
+)
+
+app_name = 'datasets'
+urlpatterns = [
+    path('', view=DatasetListView.as_view(), name='dataset-list'),
+    path('text/create/', view=TextDatasetCreateView.as_view(), name='text-dataset-create'),
+    path('text/<slug:dataset_slug>/', view=TextDatasetDetailView.as_view(), name='text-dataset-detail'),
+]
